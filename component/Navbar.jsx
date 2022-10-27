bfb970ef8e7610ad46f6c4afd05d71be96560266
Biswajit sikdar <biswajitsikdar51@gmail.com>
2022-10-28 02:52:28 +0600

    Next js navbar Design

---
+++ component/Navbar.jsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import styles from "../styles/Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.item}>
+          <div className={styles.callButton}>
+            <Image src="/img/telephone.png" alt="" width="32" height="32" />
+          </div>
+          <div className={styles.text}>
+            <div className={styles.text}>Order Now</div>
+            <div className={styles.text}>012 345 678</div>
+          </div>
+        </div>
+        <div className={styles.item}>
+          <ul className={styles.list}>
+            <li className={styles.listItem}>Home</li>
+            <li className={styles.listItem}>Product</li>
+            <li className={styles.listItem}>Menu</li>
+            <Image src="/img/logo.png" alt=".." width="160" height="69" />
+            <li className={styles.listItem}>Event</li>
+            <li className={styles.listItem}>Blog</li>
+            <li className={styles.listItem}>Contact</li>
+          </ul>
+        </div>
+        <div className={styles.item}>
+          <div className={styles.cart}>
+            <Image src="/img/cart.png" alt=".." width="30" height="30" />
+            <div className={styles.counter}>2</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import Footer from "../component/Footer";
+import Navbar from "../component/Navbar";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
